d9ab282bcfdf9625cafdbf0cc1a6b4db764d541a
Unknown <danielhey@outlook.com>
2018-08-24 20:00:25 +1000

    Linux compatible

--- setup.py
+++ setup.py
@@ -7,17 +7,20 @@ from setuptools import setup, Extension
 
 # include_dirs = [".", tf.sysconfig.get_include()]
 # include_dirs.append(os.path.join(
-#     include_dirs[1], "external/nsync/public"))
+#      include_dirs[1], "external/nsync/public"))
 
 compile_flags = tf.sysconfig.get_compile_flags()
-#compile_flags += ["-std=c++11", "-stdlib=libc++", "-O2", "-undefined dynamic_lookup"]
-compile_flags += ["-std=c++11", "-stdlib=libc++", "-O2"]
+compile_flags += ["-std=c++11", 
+                #"-stdlib=libc++", # This one won't work on clang Linux?
+                "-O2",
+                #"-undefined dynamic_lookup"
+                ]
 link_flags = tf.sysconfig.get_link_flags()
 
 extensions = [
     Extension(
         "maelstrom.kepler.kepler_op",
-        sources=["maelstrom/kepler/kepler_op.cc"],ß
+        sources=["maelstrom/kepler/kepler_op.cc"],
         language="c++",
         extra_compile_args=compile_flags,
         extra_link_args=link_flags,
