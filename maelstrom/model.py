60626f7b1895b963faa18006c54333f8edea0558
Unknown <danielhey@outlook.com>
2018-08-24 19:59:49 +1000

    Integrated optimizer + instantiating from MAST

--- maelstrom/model.py
+++ maelstrom/model.py
@@ -6,6 +6,7 @@ __all__ = ["InterpMaelstrom"]
 
 import numpy as np
 import tensorflow as tf
+import tqdm
 
 from .interp import interp
 from .maelstrom import Maelstrom
@@ -33,3 +34,43 @@ class InterpMaelstrom(Maelstrom):
         self.tau = self.psi[:, None] + tf.zeros((len(self.time_data),
                                                  len(self.nu_data)),
                                                 dtype=self.T)
+
+    @staticmethod
+    def from_mast(target, **kwargs):
+        """Instantiates a Maelstrom object from target ID by downloading
+        photometry from MAST
+        """
+        try:
+            from lightkurve import KeplerLightCurveFile
+        except ImportError:
+            raise ImportError('Lightkurve package is required for MAST')
+        
+        lcs = KeplerLightCurveFile.from_archive(target, quarter='all', 
+                                                cadence='long')
+        lc = lcs[0].PDCSAP_FLUX.remove_nans()
+        lc.flux = -2.5 * np.log10(lc.flux)
+        lc.flux = lc.flux - np.average(lc.flux)
+        for i in lcs[1:]:
+            i = i.PDCSAP_FLUX.remove_nans()
+            i.flux = -2.5 * np.log10(i.flux)
+            i.flux = i.flux - np.average(i.flux)
+            lc = lc.append(i)
+
+        return InterpMaelstrom(lc.time, lc.flux, **kwargs)
+
+    def to_maelstrom(self):
+        return Maelstrom(self.time_data, self.mag_data, self.nu_data)
+        
+    def adam_optimizer(self, params=None, steps=1000):
+        if params is None:
+            params = self.params
+        
+        opt = tf.train.AdamOptimizer().minimize(
+            self.chi2, var_list=self.params
+        )
+        self.run(tf.global_variables_initializer())
+
+        bar = tqdm.trange(steps)
+        for i in bar:
+            chi2, _ = self.run([self.chi2, opt])
+            bar.set_postfix(chi2="{0:.0f}".format(chi2))
